c28054df2339e2f14560058328ae9185417f277e
boyk0 <v_boiko-itssv@kw.com>
2023-01-29 19:02:20 +0200

    home content

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,7 +1,11 @@
+import { useTranslation } from 'react-i18next';
+
 export const Home = () => {
+	const { t } = useTranslation();
+
 	return (
 		<div className="Home">
-			Home page
+			{t('Home content')}
 		</div>
 	);
 }
